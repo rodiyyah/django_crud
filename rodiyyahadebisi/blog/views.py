7324404cb1828c00afcaa09ac0f395802d3e395b
rodiyyah <105185447+rodiyyah@users.noreply.github.com>
2022-06-24 01:09:52 +0100

    edited

--- rodiyyahadebisi/blog/views.py
+++ rodiyyahadebisi/blog/views.py
@@ -9,24 +9,28 @@ from.models import Post
 # Create your views here.
 class PostListView(ListView):
     model = Post
+    template_name = "templates/blog/post_form.html"
 
 class PostCreateView(CreateView):
     model = Post
     fields = "__all__"
     success_url  = reverse_lazy('blog:all')
-    template_name = "post_form.html"
+    template_name = "templates/blog/post_form.html"
 
 class PostDetailView(DetailView):
     model = Post
+    template_name = "templates/blog/post_detail.html"
 
 class PostUpdateView(UpdateView):
     model = Post
     fields = "__all__"
     success_url  = reverse_lazy("blog:all")
+    template_name = "templates/blog/post_form.html"
 
 class PostDeleteView(DeleteView):
     model = Post
     fields = "__all__"
     success_url  = reverse_lazy("blog:all")
+    template_name = "templates/blog/post_confirm_delete.html"
 
 
